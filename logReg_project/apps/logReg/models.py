074c765d08b5ae060c79abde4417b36ccc11cb05
Patrick Hill <patrick.hill8991@gmail.com>
2018-06-18 23:11:51 -0700

    Initial validation finished

--- logReg_project/apps/logReg/models.py
+++ logReg_project/apps/logReg/models.py
@@ -1,13 +1,73 @@
 from django.db import models
+import re
+
+REGEX_EMAIL = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+
 
 class UserManager(models.Manager):
-    def basic_validator(self, postData):
-        errors = {}
+    def registration_validator(self, postData):
+        valid = True
+        errors = {
+            "first_name"    : [],
+            "last_name"     : [],
+            "email"         : [],
+            "password"      : []
+        }
+        
+        #first and last name validation
         if len(postData["first_name"]) < 2:
-            errors["first_name"] = "First name must be at least 2 characters"
+            errors["first_name"].append("First name must be at least 2 characters")
+            valid = False
+        if not postData["first_name"].isalpha():
+            errors["first_name"].append("First name can only contain letters")
+            valid = False
+
         if len(postData["last_name"]) < 2:
-            errors["last_name"] = "Last name must be at least 2 characters"
-        return errors
+            errors["last_name"].append("Last name must be at least 2 characters")
+            valid = False
+        if not postData["last_name"].isalpha():
+            errors["last_name"].append("Last name can only contain letters")
+            valid = False
+            
+        #email validation
+        if len(postData["email"]) < 1:
+            errors["email"].append("No email provided")
+            valid = False
+        elif not re.match(REGEX_EMAIL,postData["email"]):
+            errors["email"].append("Invalid email address")
+            valid = False
+
+        #password validation
+        if len(postData["password"]) < 8:
+            errors["password"].append("Password must contain at least 8 characters")
+            valid = False
+        elif postData["password"] != postData["password_confirm"]:
+            errors["password"].append("Passwords do not match")
+            valid = False
+        
+        if valid == False:
+            return errors
+
+        return False
+
+    def login_validator(self,postData):
+        valid = True
+        errors = {}
+
+        #check if email in database
+        if not User.objects.filter(email=postData["email"]).exists():
+            valid = False
+        else:
+            #compare password form data to database
+            user = User.objects.get(email=postData["email"])
+            if user.password != postData["password"]:
+                valid = False
+            
+        if not valid:
+            errors["login"] = "Invalid username or password"
+            return errors
+
+        return False
 
 # Create your models here.
 class User(models.Model):
--- logReg_project/apps/logReg/views.py
+++ logReg_project/apps/logReg/views.py
@@ -4,14 +4,28 @@ from .models import *
 
 # Create your views here.
 def index(request):
+    #save session variables
+    if "first_name" not in request.session:
+        request.session["first_name"]   = ""
+        request.session["last_name"]    = ""
+        request.session["email"]        = ""
+
     return render(request, "logReg/index.html")
 
 def register(request):
-    errors = User.objects.basic_validator(request.POST)
-    if len(errors):
-        for tag, error in errors.items():
-            messages.error(request, error, extra_tags=tag)
-            return redirect("/")
+    errors = User.objects.registration_validator(request.POST)
+
+    if errors:
+        #save session variables
+        request.session["first_name"]   = request.POST["first_name"]
+        request.session["last_name"]    = request.POST["last_name"]
+        request.session["email"]        = request.POST["email"]
+
+        #add errors to messages.error
+        for key in errors:
+            for msg in errors[key]:
+                messages.error(request, msg, extra_tags="registration")
+        return redirect("/")
     else:
         if request.method == "POST":
             first_name  = request.POST["first_name"]
@@ -22,9 +36,28 @@ def register(request):
             #create new user
             User.objects.create(first_name=first_name, last_name=last_name,email=email,password=password)
 
+            #store first name in session
+            request.session["first_name"]   = first_name
+
+            #success message
+            messages.success(request, "Successfully registered (or logged in)!")
+
     return redirect("/success")
 
 def login(request):
+    errors = User.objects.login_validator(request.POST)
+
+    if errors:
+        for error in errors:
+            messages.error(request, errors[error],extra_tags="login")
+        return redirect("/")
+    else:
+        if request.method == "POST":
+            user = User.objects.get(email=request.POST["email"])
+            request.session["first_name"] = user.first_name
+            
+            #success message
+            messages.success(request, "Successfully registered (or logged in)!")
     return redirect("/success")
 
 def success(request):
